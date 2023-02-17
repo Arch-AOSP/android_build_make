7bd10fef4b3993e59e31bb48e7d059292f02fc3b
pixelodin <khal1d@protonmail.com>
2023-02-17 18:42:44 +0000

    include Updater app when OFFICIAL_BUILD=true
    Change-Id: I112091f8c5d687e9914a4adec2a0729e93aaca60

--- target/product/media_system.mk
+++ target/product/media_system.mk
@@ -36,6 +36,11 @@ PRODUCT_PACKAGES += \
     requestsync \
     StatementService \
 
+
+ifeq ($(OFFICIAL_BUILD),true)
+    PRODUCT_PACKAGES += Updater
+endif
+
 PRODUCT_HOST_PACKAGES += \
     fsck.f2fs \
 
